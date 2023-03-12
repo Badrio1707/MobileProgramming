7345ffd20a584f70641ebe77e7a1f8848ccf75bc
Muhamad Badrio Taupani <bcrashbussines@gmail.com>
2023-03-12 07:02:42 +0700

    commit

---
+++ BlankApp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BlankApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
